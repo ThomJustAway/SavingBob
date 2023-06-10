7f615bbfad452c4577ff49bee584233a24303144
Thom low <thomthomlow@gmail.com>
2023-06-10 09:32:59 +0800

    10 June 2023
    Fix the animation and putting the button for the gears

---
+++ unititle_Game_project_prototype/Assets/tryoutFolder/UI/GearMenuBehaviour.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearMenuBehaviour : MonoBehaviour
+{
+    private RectTransform rectTransform;
+    private bool isActivated= false;
+    private Animator animator;
+    private int isActivatedHash = Animator.StringToHash("IsActivated");
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void OnClick()
+    {
+        animator.SetBool(isActivatedHash, !animator.GetBool(isActivatedHash));
+    }
+
+
+
+}
