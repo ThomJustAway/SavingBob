1174a69d13b8c257077e833578799519fe982e12
Thom low <thomthomlow@gmail.com>
2023-06-14 21:24:58 +0800

    14 june 2023
    Improvement to the gear button and added icon

--- unititle_Game_project_prototype/Assets/tryoutFolder/UI/GearButton.cs
+++ unititle_Game_project_prototype/Assets/tryoutFolder/UI/GearButton.cs
@@ -1,22 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class GearButton : MonoBehaviour
 {
     [SerializeField]private GameObject placeableGear;
     [SerializeField]private int numberOfGear = 10;
+    [SerializeField] private string nameOfButton;
+    [SerializeField] private GameObject imageContainer;
 
+    [Range(5,50)] // figure out how to make the gameobject change with the image?
+    [SerializeField] private float scale = 37f;
     private Queue<GameObject> pooledGear = new Queue<GameObject>();
     private GameObject gearParent;
-    [SerializeField] private string nameOfButton;
     private void Start()
     {
-        TextMeshPro gearButtonNameDisplay = gameObject.GetComponentInParent<TextMeshPro>();
-        gearButtonNameDisplay.text = nameOfButton;
+        SettingUpButton();
+
         //GameObject gearImage;
         AddingGearToPool();
+    }
+
+    private void SettingUpButton()
+    {
+        TextMeshProUGUI gearButtonNameDisplay = gameObject.GetComponentInChildren<TextMeshProUGUI>();
+        gearButtonNameDisplay.text = nameOfButton; //setting the name
+
+        Transform imageGearObject = Instantiate(placeableGear, imageContainer.transform).transform;
+
+        foreach (Transform child in imageGearObject) //destroy the component inside the gameobject
+        {
+            print(child.name);
+            Destroy(child.gameObject);
+        }
+        Gear gearComponent=imageGearObject.GetComponent<Gear>();
+        Destroy(gearComponent); 
+        Collider2D colliderComponent = gameObject.GetComponent<Collider2D>();
+        Destroy(colliderComponent);
+        RectTransform rectTransform=imageGearObject.AddComponent<RectTransform>();
+        rectTransform.localScale = new Vector3(scale, scale, 0);
     }
 
     private void AddingGearToPool()
